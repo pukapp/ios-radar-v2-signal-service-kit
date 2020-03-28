3c48945acc3bd4dc46577318cb7d8882963f1051
houdongbing <dongbinghouy@gmail.com>
2020-03-28 16:50:43 +0800

    feat(AI_Trainer): update TSTrainerThread.h  TSTrainerThread.m

--- src/Contacts/Threads/TSTrainerThread.h
+++ src/Contacts/Threads/TSTrainerThread.h
@@ -6,9 +6,53 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface TSTrainerThread : TSContactThread
+extern NSString *const TSTrainerThreadPrefix;
+
+@class SignalServiceAddress;
+
+@interface TSTrainerThread : TSThread
+
+
+- (instancetype)initWithUniqueId:(NSString *)uniqueId
+                    archivalDate:(nullable NSDate *)archivalDate
+       archivedAsOfMessageSortId:(nullable NSNumber *)archivedAsOfMessageSortId
+           conversationColorName:(ConversationColorName)conversationColorName
+                    creationDate:(nullable NSDate *)creationDate
+isArchivedByLegacyTimestampForSorting:(BOOL)isArchivedByLegacyTimestampForSorting
+                 lastMessageDate:(nullable NSDate *)lastMessageDate
+                    messageDraft:(nullable NSString *)messageDraft
+                  mutedUntilDate:(nullable NSDate *)mutedUntilDate
+           shouldThreadBeVisible:(BOOL)shouldThreadBeVisible
+              hasDismissedOffers:(BOOL)hasDismissedOffers
+NS_SWIFT_NAME(init(uniqueId:archivalDate:archivedAsOfMessageSortId:conversationColorName:creationDate:isArchivedByLegacyTimestampForSorting:lastMessageDate:messageDraft:mutedUntilDate:shouldThreadBeVisible:hasDismissedOffers:));
+
 
 - (instancetype)initWithContactId:(NSString *)contactId;
+
+@property (nonatomic) BOOL hasDismissedOffers;
+
++ (instancetype)getOrCreateThreadWithContactId:(NSString *)contactId NS_SWIFT_NAME(getOrCreateThread(contactId:));
+
++ (instancetype)getOrCreateThreadWithContactId:(NSString *)contactId
+                                   transaction:(YapDatabaseReadWriteTransaction *)transaction;
+
++ (instancetype)getOrCreateThreadWithContactId:(NSString *)contactId
+                                anyTransaction:(SDSAnyWriteTransaction *)transaction;
+
+// Unlike getOrCreateThreadWithContactId, this will _NOT_ create a thread if one does not already exist.
++ (nullable instancetype)getThreadWithContactId:(NSString *)contactId transaction:(YapDatabaseReadTransaction *)transaction;
++ (nullable instancetype)getThreadWithContactId:(NSString *)contactId
+                                 anyTransaction:(SDSAnyReadTransaction *)transaction;
+
+- (NSString *)contactIdentifier;
+- (SignalServiceAddress *)contactAddress;
+
++ (NSString *)contactIdFromThreadId:(NSString *)threadId;
+
++ (NSString *)threadIdFromContactId:(NSString *)contactId;
+
++ (NSString *)conversationColorNameForRecipientId:(NSString *)recipientId
+                                      transaction:(SDSAnyReadTransaction *)transaction;
 
 @end
 
--- src/Contacts/Threads/TSTrainerThread.m
+++ src/Contacts/Threads/TSTrainerThread.m
@@ -3,25 +3,171 @@
 // 
 
 #import "TSTrainerThread.h"
+#import "ContactsManagerProtocol.h"
+#import "ContactsUpdater.h"
+#import "NotificationsProtocol.h"
+#import "OWSIdentityManager.h"
+#import "SSKEnvironment.h"
+#import <SignalServiceKit/SignalServiceKit-Swift.h>
+#import <YapDatabase/YapDatabaseConnection.h>
+#import <YapDatabase/YapDatabaseTransaction.h>
+
+NS_ASSUME_NONNULL_BEGIN
 
 NSString *const TSTrainerThreadPrefix = @"t";
 
 @implementation TSTrainerThread
 
-- (instancetype)initWithContactId:(NSString *)contactId
+- (instancetype)initWithUniqueId:(NSString *)uniqueId
+                    archivalDate:(nullable NSDate *)archivalDate
+       archivedAsOfMessageSortId:(nullable NSNumber *)archivedAsOfMessageSortId
+           conversationColorName:(ConversationColorName)conversationColorName
+                    creationDate:(nullable NSDate *)creationDate
+isArchivedByLegacyTimestampForSorting:(BOOL)isArchivedByLegacyTimestampForSorting
+                 lastMessageDate:(nullable NSDate *)lastMessageDate
+                    messageDraft:(nullable NSString *)messageDraft
+                  mutedUntilDate:(nullable NSDate *)mutedUntilDate
+           shouldThreadBeVisible:(BOOL)shouldThreadBeVisible
+              hasDismissedOffers:(BOOL)hasDismissedOffers
 {
-    NSString *uniqueIdentifier = [TSTrainerThreadPrefix stringByAppendingString:contactId];
+    self = [super initWithUniqueId:uniqueId
+                      archivalDate:archivalDate
+         archivedAsOfMessageSortId:archivedAsOfMessageSortId
+             conversationColorName:conversationColorName
+                      creationDate:creationDate
+isArchivedByLegacyTimestampForSorting:isArchivedByLegacyTimestampForSorting
+                   lastMessageDate:lastMessageDate
+                      messageDraft:messageDraft
+                    mutedUntilDate:mutedUntilDate
+             shouldThreadBeVisible:shouldThreadBeVisible];
+    
+    if (!self) {
+        return self;
+    }
+    
+    _hasDismissedOffers = hasDismissedOffers;
+    
+    return self;
+}
+
+
+- (instancetype)initWithContactId:(NSString *)contactId {
+    NSString *uniqueIdentifier = [[self class] threadIdFromContactId:contactId];
     
     OWSAssertDebug(contactId.length > 0);
+    
     self = [super initWithUniqueId:uniqueIdentifier];
     
     return self;
 }
 
++ (instancetype)getOrCreateThreadWithContactId:(NSString *)contactId
+                                   transaction:(YapDatabaseReadWriteTransaction *)transaction {
+    OWSAssertDebug(contactId.length > 0);
+    
+    TSTrainerThread *thread =
+    [self fetchObjectWithUniqueID:[self threadIdFromContactId:contactId] transaction:transaction];
+    
+    if (!thread) {
+        thread = [[TSTrainerThread alloc] initWithContactId:contactId];
+        [thread saveWithTransaction:transaction];
+    }
+    
+    return thread;
+}
+
++ (instancetype)getOrCreateThreadWithContactId:(NSString *)contactId
+                                anyTransaction:(SDSAnyWriteTransaction *)transaction
+{
+    OWSAssertDebug(contactId.length > 0);
+    
+    NSString *uniqueId = [self threadIdFromContactId:contactId];
+    TSTrainerThread *thread = (TSTrainerThread *)[self anyFetchWithUniqueId:uniqueId transaction:transaction];
+    
+    if (!thread) {
+        thread = [[TSTrainerThread alloc] initWithContactId:contactId];
+        [thread anyInsertWithTransaction:transaction];
+    }
+    
+    return thread;
+}
+
++ (instancetype)getOrCreateThreadWithContactId:(NSString *)contactId
+{
+    OWSAssertDebug(contactId.length > 0);
+    
+    __block TSTrainerThread *thread;
+    [[self dbReadWriteConnection] readWriteWithBlock:^(YapDatabaseReadWriteTransaction *transaction) {
+        thread = [self getOrCreateThreadWithContactId:contactId transaction:transaction];
+    }];
+    
+    return thread;
+}
+
++ (nullable instancetype)getThreadWithContactId:(NSString *)contactId
+                                    transaction:(YapDatabaseReadTransaction *)transaction
+{
+    return [TSTrainerThread fetchObjectWithUniqueID:[self threadIdFromContactId:contactId] transaction:transaction];
+}
+
++ (nullable instancetype)getThreadWithContactId:(NSString *)contactId
+                                 anyTransaction:(SDSAnyReadTransaction *)transaction
+{
+    return (TSTrainerThread *)[TSTrainerThread anyFetchWithUniqueId:[self threadIdFromContactId:contactId]
+                                                        transaction:transaction];
+}
+
+- (NSString *)contactIdentifier {
+    return [[self class] contactIdFromThreadId:self.uniqueId];
+}
+
+- (SignalServiceAddress *)contactAddress
+{
+    return [[SignalServiceAddress alloc] initWithPhoneNumber:self.contactIdentifier];
+}
+
+- (NSArray<NSString *> *)recipientIdentifiers
+{
+    return @[self.contactIdentifier];
+}
+
+- (BOOL)isGroupThread {
+    return false;
+}
+
+- (BOOL)hasSafetyNumbers
+{
+    return !![[OWSIdentityManager sharedManager] identityKeyForRecipientId:self.contactIdentifier];
+}
+
+
 - (NSString *)name
 {
-    return @"训练对话";
-//    return [SSKEnvironment.shared.contactsManager displayNameForPhoneIdentifier:self.contactIdentifier];
+    return [SSKEnvironment.shared.contactsManager displayNameForPhoneIdentifier:self.contactIdentifier];
+}
+
++ (NSString *)threadIdFromContactId:(NSString *)contactId {
+    return [TSTrainerThreadPrefix stringByAppendingString:contactId];
+}
+
++ (NSString *)contactIdFromThreadId:(NSString *)threadId {
+    return [threadId substringWithRange:NSMakeRange(1, threadId.length - 1)];
+}
+
++ (NSString *)conversationColorNameForRecipientId:(NSString *)recipientId
+                                      transaction:(SDSAnyReadTransaction *)transaction
+{
+    OWSAssertDebug(recipientId.length > 0);
+    
+    TSTrainerThread *_Nullable contactThread =
+    [TSTrainerThread getThreadWithContactId:recipientId anyTransaction:transaction];
+    if (contactThread) {
+        return contactThread.conversationColorName;
+    }
+    return [self stableColorNameForNewConversationWithString:recipientId];
 }
 
 @end
+
+NS_ASSUME_NONNULL_END
+
